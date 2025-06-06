2d9a72958327716c5a59981f1318ba6e4668b45e
chalita <chalitayamroll@gmail.com>
2025-06-06 23:43:04 +0700

    style: Update CSS styles for WeatherArticle, WeeklyArticle, and cardDailyArticle components for improved layout and spacing refactor: Clean up CampStay component by removing unused imports and adjusting layout

--- src/pages/campStay.jsx
+++ src/pages/campStay.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import SearchCampStay from '../components/SearchCampStay';
 import { Outlet, useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import Weather from '../components/Weather';
-import TabProvinces from '../components/TabProvinces';
 import RecommendedStay from "../components/RecommendedStay";
 import RecommendedCamp from '../components/RecommendedCamp';
 
 
 function CampStay() {
   const { id } = useParams();
-  const { t } = useTranslation();
 
   if (!id) {
     return (
       <>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '2rem' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
           <SearchCampStay />
         </div>
        
